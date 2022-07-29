a1d9dc08bd04e6bbf0dd40d0d49f58f6b1feaf40
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2022-07-30 01:42:02 +0600

    modified the dashboard file

---
+++ view/dashboard.php
@@ -0,0 +1,24 @@
+<?php
+ 	session_start();
+ 	if (!isset($_SESSION['username']) or empty($_SESSION['username'])) {
+ 		header("Location: login.php");
+ 	}  
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Welcome</title>
+</head>
+<body>
+
+	<h1>Welcome</h1>
+
+	<br><br>
+
+
+	<a href="../controller/Logout.php">Logout</a>
+
+</body>
+</html>
