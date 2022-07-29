acf65dcc679a6be9d74403e9e0ace63d626fc6f6
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2022-07-29 23:29:45 +0600

    Added a new file called loginAction.php

---
+++ controller/loginAction.php
@@ -0,0 +1,40 @@
+<?php
+
+	session_start();
+
+	require "../model/User.php";
+	
+	if ($_SERVER['REQUEST_METHOD'] === "POST") {
+
+		$username = sanitize($_POST['uname']);
+		$password = sanitize($_POST['password']);
+		$_SESSION['msg'] = "";
+		if (empty($username) or empty($password)) {
+			/*echo "Please fill up the form properly";*/
+			$_SESSION['msg'] = "Please fill up the form properly";
+				header("Location: ../view/login.php");
+		}
+		else {
+			$isValid = validate($username, $password);
+
+			if ($isValid) {
+				/*echo "Login Successful";*/
+				$_SESSION['msg'] = "";
+				$_SESSION['username'] = $username;
+				header("Location: ../view/welcome.php");
+			}
+			else {
+				/*echo "Login Failed";*/
+				$_SESSION['msg'] = "Login Failed...!";
+				header("Location: ../view/login.php");
+			}
+		}
+	}
+
+	function sanitize($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+?>
