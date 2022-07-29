9b071d7d03aef0402335cd18babcd780ab774d37
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2022-07-29 17:42:42 +0600

    Updated the registration.php file

--- view/registration.php
+++ view/registration.php
@@ -10,6 +10,24 @@
 
 	<form method="post" action="../controller/regAction.php" novalidate>
 
+		<label for="fname">First name:</label>
+		<input type="text" name="fname" id="fname">
+		<span id="fnameErrorMsg"></span>
+		<br><br>
+
+		<label for="lname">Last name:</label>
+		<input type="text" name="lname" id="lname">
+		<span id="lnameErrorMsg"></span>
+		<br><br>
+
+		<label>Gender</label>
+		<input type="radio" name="gender" id="male" value="male">
+		<label for="male">Male</label>
+		<input type="radio" name="gender" id="female" value="female">
+		<label for="female">Female</label>
+		<br>
+		<span id = "genderErrMsg"></span><br><br>
+
 		<label for="uname">Username:</label>
 		<input type="text" name="uname" id="uname">
 		<span id="unameErrorMsg"></span>
@@ -18,6 +36,17 @@
 		<label for="password">Password:</label>
 		<input type="password" name="password" id="password"> 
 		<span id="passwordErrorMsg"></span>
+		<br><br>
+
+		<label for="role">Role</label>
+		<select name="role" id="role">
+			<option selected disabled> Select and option</option>
+			<option value="receiver">Receiver</option>
+			<option value="donor">Donor</option>
+			<option value="staff">Staff</option>
+			<option value="admin">Admin</option>
+		</select>
+		<span id= "roleErr"></span>
 		<br><br>
 
 		<input type="submit" name="submit">
