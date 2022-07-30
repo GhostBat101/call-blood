1b83eebff99066590844900c2663b5134d700785
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2022-07-30 15:56:49 +0600

    added the header file

---
+++ view/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Call Blood</title>
+	<link rel="stylesheet" href="../view/css/style.css">
+</head>
+<body>
+	<header>
+		<fieldset>
+		<h1 style="color: red;">Welcome To Call-Blood </h1>
+		<a href="../view/dashboard.php"><button type="button" >Dashboard</button></a>
+		<a href="../view/profile.php"><button type="button">Profile</button></a>
+		<a href="../controller/Logout.php"><button type="button">Logout</button></a>
+		<br><br>
+	</fieldset><br>
+	</header>
+	
+</body>
+</html>
